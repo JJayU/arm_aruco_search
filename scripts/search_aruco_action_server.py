ef89e25a699edf9220c62f537404efe9d762db8b
JJayU <jakub.junkiert@gmail.com>
2024-11-05 22:07:12 +0000

    Initial commit

---
+++ scripts/search_aruco_action_server.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+import time
+
+import rclpy
+from rclpy.action import ActionServer
+from rclpy.node import Node
+
+from search_aruco_action.action import SearchArUco
+
+
+class SearchArucoActionServer(Node):
+
+    def __init__(self):
+        super().__init__('search_aruco_action_server')
+
+        self._action_server = ActionServer(
+            self,
+            SearchArUco,
+            'SearchAruco',
+            self.execute_callback)
+            
+        self.get_logger().info('SearchAruco Action Server is running...')
+
+    def execute_callback(self, goal_handle):
+        self.get_logger().info('Executing goal...')
+
+        feedback_msg = SearchArUco.Feedback()
+        feedback_msg.partial_locations_x = []
+        feedback_msg.partial_locations_y = []
+
+        for i in range(1, int(goal_handle.request.timeout)):
+            feedback_msg.partial_locations_x.append(i)
+            self.get_logger().info('Feedback: {0}'.format(feedback_msg.partial_locations_x))
+            goal_handle.publish_feedback(feedback_msg)
+            time.sleep(1)
+
+        goal_handle.succeed()
+
+        result = SearchArUco.Result()
+        result.locations_x = feedback_msg.partial_locations_x
+        result.locations_y = feedback_msg.partial_locations_y
+        return result
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    search_aruco_action_server = SearchArucoActionServer()
+
+    rclpy.spin(search_aruco_action_server)
+
+
+if __name__ == '__main__':
+    main()
